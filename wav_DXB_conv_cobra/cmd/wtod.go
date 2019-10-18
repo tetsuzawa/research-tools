46dfc648ea0ef7015e48302cc954267fb9273853
Tetsu Takizawa <tetsu.takizawa5@gmail.com>
2019-10-18 20:56:28 +0900

    Add multi channel format

--- wav_DXB_conv_cobra/cmd/wtod.go
+++ wav_DXB_conv_cobra/cmd/wtod.go
@@ -14,7 +14,7 @@ import (
 )
 
 type Options struct {
-	Ch      int    `validate:"min=1,max=10"`
+	Ch      int    `validate:"min=1,max=2"`
 	OutName string `validate:"excludesall=!&%*;:."`
 }
 
@@ -54,7 +54,7 @@ func wtod(cmd *cobra.Command, args []string) {
 	nameParts := strings.Split(fileName, ".")
 
 	var name string
-	fmt.Println("flags: ",o.Ch,o.OutName)
+	fmt.Println("flags: ", o.Ch, o.OutName)
 	if o.OutName != "" {
 		name = o.OutName
 	} else {
@@ -92,16 +92,19 @@ func wtod(cmd *cobra.Command, args []string) {
 		cmd.Println("type: ", reflect.TypeOf(data))
 		cmd.Println("ex: ", ex)
 
-		if value, ok := data.([]int16); ok {
+		value, ok := data.([]int16)
+		if !ok {
+			cmd.PrintErrf("Data type is not valid")
+			os.Exit(5)
+		}
 
+		if o.Ch == 1 {
 			fw, err := os.Create(name + ".DSB")
 			if err != nil {
 				cmd.PrintErrln(err)
 				os.Exit(3)
 			}
-			cmd.Println("len of value: ", len(value))
 			defer fw.Close()
-
 			b := make([]byte, 2)
 			buf := make([]byte, 0)
 
@@ -117,13 +120,47 @@ func wtod(cmd *cobra.Command, args []string) {
 				cmd.PrintErrln(err)
 				os.Exit(3)
 			}
+			cmd.Printf("\n\n")
+			cmd.Println("end!!")
 		} else {
-			cmd.PrintErrln(err)
-			os.Exit(2)
-		}
-		cmd.Printf("\n\n")
-		cmd.Println("end!!")
+			fw1, err := os.Create(name + "ch1.DSB")
+			if err != nil {
+				cmd.PrintErrln(err)
+				os.Exit(3)
+			}
+			defer fw1.Close()
+			fw2, err := os.Create(name + "ch2.DSB")
+			if err != nil {
+				cmd.PrintErrln(err)
+				os.Exit(3)
+			}
+			defer fw2.Close()
+			b := make([]byte, 2)
+			buf1 := make([]byte, 0)
+			buf2 := make([]byte, 0)
+			for i := 0; i < len(value)/2; i++ {
+				cmd.Printf("working... %d%%\r", (i+1)*100*2/len(value))
 
+				ui1 := converter.Int16ToUint16(value[i*2])
+				ui2 := converter.Int16ToUint16(value[i*2+1])
+				binary.LittleEndian.PutUint16(b, ui1)
+				binary.LittleEndian.PutUint16(b, ui2)
+				buf1 = append(buf1, b...)
+				buf2 = append(buf2, b...)
+			}
+			_, err = fw1.Write(buf1)
+			if err != nil {
+				cmd.PrintErrln(err)
+				os.Exit(3)
+			}
+			_, err = fw2.Write(buf2)
+			if err != nil {
+				cmd.PrintErrln(err)
+				os.Exit(3)
+			}
+			cmd.Printf("\n\n")
+			cmd.Println("end!!")
+		}
 	case "DSB":
 		buff, err := ioutil.ReadAll(f)
 		if err != nil {
