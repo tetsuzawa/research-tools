42367625a1b4fc448e4e1a66dd5cbee59fb21337
Tetsu Takizawa <tetsu.takizawa5@gmail.com>
2019-11-17 14:50:19 +0900

    Add cmd

---
+++ cmd/wav_to_DSB/go.mod
@@ -0,0 +1,9 @@
+module github.com/tetsuzawa/go-research/tools/cmd/wav_to_DSB
+
+go 1.13
+
+require (
+	github.com/pkg/errors v0.8.1
+	github.com/tetsuzawa/go-wav v0.0.0-20190213080141-3d6c7c9313ea
+	github.com/urfave/cli v1.22.1
+)
---
+++ cmd/wav_to_DSB/main.go
@@ -0,0 +1,64 @@
+/*
+Contents: wav to DSB converter.
+	This program converts .wav file to .DSB files.
+	Please run `wav_to_DSB --help` for details.
+Usage: wav_to_DSB (-o /path/to/out.DSB) /path/to/file.wav
+Author: Tetsu Takizawa
+E-mail: tt15219@tomakomai.kosen-ac.jp
+LastUpdate: 2019/11/16
+DateCreated  : 2019/11/16
+*/
+package main
+
+import (
+	"github.com/pkg/errors"
+	"os"
+
+	"github.com/urfave/cli"
+)
+
+func main() {
+	app := cli.NewApp()
+	defer app.Run(os.Args)
+	app.CustomAppHelpTemplate = HelpTemplate
+
+	app.Name = "wav_to_DSB"
+	app.Usage = `This app converts .wav file to .DSB file.`
+	app.Version = "0.1.0"
+
+	app.Action = action
+
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "outpath, o",
+			Usage: "specify output path like as /path/to/file",
+		},
+	}
+}
+
+func action(ctx *cli.Context) error {
+	if ctx.Args().Get(0) == "" {
+		return cli.NewExitError("too few arguments. need input file path. \nUsage: wav-to-DSB-multi /path/to/file.wav", 2)
+	}
+
+	fileName := ctx.Args().Get(0)
+	name, ext := splitPathAndExt(fileName)
+
+	if ext != ".wav" {
+		return cli.NewExitError("incorrect file format. need .wav file. \nUsage: wav-to-DSB-multi /path/to/file.wav", 2)
+	}
+
+	if ctx.String("o") != "" {
+		argName := ctx.String("o")
+		name, _ = splitPathAndExt(argName)
+	}
+
+	f, err := os.Open(fileName)
+	if err != nil {
+		err = errors.Wrap(err, "error occurred while opening input file")
+		return cli.NewExitError("no such a file", 2)
+	}
+	defer f.Close()
+
+	return wavToDSB(ctx, f, name)
+}
