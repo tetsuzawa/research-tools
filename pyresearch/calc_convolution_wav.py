20a8d66adca9cdfb88d23980439549f21868384d
Tetsu Takizawa <tetsu.takizawa5@gmail.com>
2020-01-11 15:31:37 +0900

    Fix bug

--- pyresearch/calc_convolution_wav.py
+++ pyresearch/calc_convolution_wav.py
@@ -10,7 +10,7 @@ import soundfile as sf
 
 def main():
     description = "This script calculates the convolution of two wav files."
-    usage = f"Usage: python {__file__} [-t full|valid|same] name1.wav name2.wav /path/to/savedir"
+    usage = f"Usage: python {__file__} [-m full|valid|same] name1.wav name2.wav /path/to/output_name.wav"
 
     parser = argparse.ArgumentParser(usage=usage, description=description)
 
@@ -22,7 +22,7 @@ def main():
                         help='paths where the wav file is located.',
                         )
 
-    parser.add_argument('-d', '--output_path',
+    parser.add_argument('-o', '--output_path',
                         nargs='?',
                         const="/tmp/conv.wav",
                         default="conv.wav",
@@ -30,7 +30,7 @@ def main():
                         help='Output file path where you want to locate wav file. (default: current directory)',
                         )
 
-    parser.add_argument('-t', '--type',
+    parser.add_argument('-m', '--mode',
                         action='store',
                         nargs='?',
                         default="full",
@@ -43,16 +43,18 @@ def main():
 
     input_paths = args.input_paths
     output_path = pathlib.Path(args.output_path)
-    type = args.type
+    mode = args.mode
 
     if len(input_paths) != 2:
-        print("Error: number of arguments is invalid")
-        print(f"Usage: python {__file__} [-t full|valid|same] name1.wav name2.wav /path/to/savedir ")
+        print(f"Error: number of arguments is invalid. got:{len(input_paths)}")
+        print(parser.usage)
         exit(1)
 
-    if type != "full" or type != "valid" or type != "same":
-        print("Error: type of convolution is invalid")
-        print()
+    if mode != "full" and mode != "valid" and mode != "same":
+        print(f"Error: mode of convolution is invalid. got:{mode}")
+        print(parser.usage)
+
+        exit(1)
 
     input_path_1 = input_paths[0]
     input_path_2 = input_paths[1]
@@ -63,7 +65,8 @@ def main():
     data_1, fs_1 = sf.read(input_path_1)
     data_2, fs_2 = sf.read(input_path_2)
 
-    wav_out = np.convolve(data_1, data_2, type=type)
+    print("working...")
+    wav_out = np.convolve(data_1, data_2, mode=mode)
 
     sf.write(file=str(output_path.stem + ".wav"), data=wav_out, samplerate=48000, endian="LITTLE",
              format="WAV", subtype="PCM_16")
