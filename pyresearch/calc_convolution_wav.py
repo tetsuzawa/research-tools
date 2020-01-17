90bdc521bca1fc08b46999ce0e742e9df5d7c853
Tetsu Takizawa <tetsu.takizawa5@gmail.com>
2020-01-17 10:00:11 +0900

    Fix code

--- pyresearch/calc_convolution_wav.py
+++ pyresearch/calc_convolution_wav.py
@@ -12,19 +12,16 @@ def main():
     description = "This script calculates the convolution of two wav files."
     usage = f"Usage: python {__file__} [-m full|valid|same] name1.wav name2.wav /path/to/output_name.wav"
 
+    # initial setting for command line arguments
     parser = argparse.ArgumentParser(usage=usage, description=description)
 
     parser.add_argument('input_paths',
                         nargs="*",
-                        const=None,
-                        default=None,
                         type=str,
-                        help='paths where the wav file is located.',
-                        )
+                        help='paths where the wav file is located.')
 
     parser.add_argument('-o', '--output_path',
                         nargs='?',
-                        const="/tmp/conv.wav",
                         default="conv.wav",
                         type=str,
                         help='Output file path where you want to locate wav file. (default: current directory)',
@@ -39,12 +36,14 @@ def main():
                         help='Convolution type',
                         )
 
+    # parse command line arguments
     args = parser.parse_args()
 
     input_paths = args.input_paths
     output_path = pathlib.Path(args.output_path)
     mode = args.mode
 
+    # validation of command line arguments
     if len(input_paths) != 2:
         print(f"Error: number of arguments is invalid. got:{len(input_paths)}")
         print(parser.usage)
@@ -62,12 +61,15 @@ def main():
 
     output_path = pathlib.Path(output_path)
 
+    # read audio file
     data_1, fs_1 = sf.read(input_path_1)
     data_2, fs_2 = sf.read(input_path_2)
 
+    # convolve
     print("working...")
     wav_out = np.convolve(data_1, data_2, mode=mode)
 
+    # write audio file
     sf.write(file=str(output_path.stem + ".wav"), data=wav_out, samplerate=48000, endian="LITTLE",
              format="WAV", subtype="PCM_16")
 
