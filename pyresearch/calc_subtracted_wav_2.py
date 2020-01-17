39ad6417dadf13a1737dbc779ec5e74e145605d7
Tetsu Takizawa <tetsu.takizawa5@gmail.com>
2020-01-17 10:01:12 +0900

    Remove comments

--- pyresearch/calc_subtracted_wav_2.py
+++ pyresearch/calc_subtracted_wav_2.py
@@ -1,59 +1,41 @@
 # coding: utf-8
 
-# spectral subtraction: one of noise reduction method
-#                       This is a simple python script, is not advanced one.
-#
-# Usage:
-#    specify wav file names below,
-#       infile: input wav file including noise
-#       outfile: output wav file
-#       noisefile: noise only wav file, that is some (noise only) portion of input wav edited by manual (ex: Audacity)
-#
-#     then, python3 ss1.py
-
-# Check version:
-#
-#  Python 3.6.4 win32 64bit
-#  Windows 10, 64bit
-#  numpy (1.14.0)
-#  scipy (1.0.0)
-#  librosa (0.6.0)
 
 import sys
-import numpy as np
-import scipy
+
 import librosa
+import numpy as np
+import soundfile as sf
+
 
 def main():
-    # edit following wav file name
-    noisefile = sys.argv[1]
-    infile = sys.argv[2]
-    outfile = sys.argv[3]
+    noise_file_path = sys.argv[1]
+    input_file_path = sys.argv[2]
+    output_file_path = sys.argv[3]
 
-    # load input file, and stft (Short-time Fourier transform)
-    print('load wav', infile)
-    w, sr = librosa.load(infile, sr=None, mono=True)  # keep native sr (sampling rate) and trans into mono
-    s = librosa.stft(w)  # Short-time Fourier transform
-    ss = np.abs(s)  # get magnitude
-    angle = np.angle(s)  # get phase
-    b = np.exp(1.0j * angle)  # use this phase information when Inverse Transform
+    print('input file:', input_file_path)
+    data, data_fs = librosa.load(input_file_path, sr=None, mono=True)
+    data_st = librosa.stft(data)
+    data_st_abs = np.abs(data_st)
+    angle = np.angle(data_st)
+    b = np.exp(1.0j * angle)
 
-    # load noise only file, stft, and get mean
-    print('load wav', noisefile)
-    nw, nsr = librosa.load(noisefile, sr=None, mono=True)
-    ns = librosa.stft(nw)
-    nss = np.abs(ns)
-    mns = np.mean(nss, axis=1)  # get mean
+    print('noise file:', noise_file_path)
+    noise_data, noise_fs = librosa.load(noise_file_path, sr=None, mono=True)
+    noise_data_st = librosa.stft(noise_data)
+    noise_data_st_abs = np.abs(noise_data_st)
+    mean_noise_abs = np.mean(noise_data_st_abs, axis=1)
 
-    # subtract noise spectral mean from input spectral, and istft (Inverse Short-Time Fourier Transform)
-    sa = ss - mns.reshape((mns.shape[0], 1))  # reshape for broadcast to subtract
-    sa0 = sa * b  # apply phase information
-    y = librosa.istft(sa0)  # back to time domain signal
+    subtracted_data = data_st_abs - mean_noise_abs.reshape(
+        (mean_noise_abs.shape[0], 1))  # reshape for broadcast to subtract
+    subtracted_data_phase = subtracted_data * b  # apply phase information
+    y = librosa.istft(subtracted_data_phase)  # back to time domain signal
 
     # save as a wav file
-    scipy.io.wavfile.write(outfile, sr, (y * 32768).astype(np.int16))  # save signed 16-bit WAV format
-    # librosa.output.write_wav(outfile, y , sr)  # save 32-bit floating-point WAV format, due to y is float
-    print('write wav', outfile)
+    # scipy.io.wavfile.write(output_file_path, data_fs, (y * 32768).astype(np.int16))  # save signed 16-bit WAV format
+    sf.write(file=str(output_file_path), data=y, samplerate=data_fs, endian="LITTLE", format="WAV", subtype="PCM_16")
+    print('output file:', output_file_path)
+
 
 if __name__ == '__main__':
     main()
